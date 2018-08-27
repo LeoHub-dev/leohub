c5629d8980cbf89440d37a1946e979d73fab0190
Leonardo Jimenez <jimenezleonardop@gmail.com>
2018-08-27 16:28:56 -0400

    Edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,151 +1,379 @@
-<!DOCTYPE HTML>
-<!--
-	Intensify by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
--->
-<html>
-	<head>
-		<title>Intensify by TEMPLATED</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="{{ asset('leohub/assets/css/main.css') }}" rel="stylesheet" />
-	</head>
-	<body>
+<!DOCTYPE html>
+<html lang="en">
 
-		<!-- Header -->
-			<header id="header">
-				<nav class="left">
-					<a href="#menu"><span>Menu</span></a>
-				</nav>
-				<a href="index.html" class="logo">intensify</a>
-				<nav class="right">
-					<a href="#" class="button alt">Log in</a>
-				</nav>
-			</header>
+  <head>
 
-		<!-- Menu -->
-			<nav id="menu">
-				<ul class="links">
-					<li><a href="index.html">Home</a></li>
-					<li><a href="generic.html">Generic</a></li>
-					<li><a href="elements.html">Elements</a></li>
-				</ul>
-				<ul class="actions vertical">
-					<li><a href="#" class="button fit">Login</a></li>
-				</ul>
-			</nav>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-		<!-- Banner -->
-			<section id="banner">
-				<div class="content">
-					<h1>Ipsum sed lorem</h1>
-					<p>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod<br />sed arcu cras consecuat lorem ipsum dolor sit amet.</p>
-					<ul class="actions">
-						<li><a href="#one" class="button scrolly">Get Started</a></li>
-					</ul>
-				</div>
-			</section>
+    <title>LeoHub - Realizamos tus ideas</title>
 
-		<!-- One -->
-			<section id="one" class="wrapper">
-				<div class="inner flex flex-3">
-					<div class="flex-item left">
-						<div>
-							<h3>Magna ultricies</h3>
-							<p>Morbi in sem quis dui plalorem ipsum<br /> euismod in, pharetra sed ultricies.</p>
-						</div>
-						<div>
-							<h3>Ipsum adipiscing lorem</h3>
-							<p>Tristique yonve cursus jam nulla quam<br /> loreipsu gravida adipiscing lorem</p>
-						</div>
-					</div>
-					<div class="flex-item image fit round">
-						<img src="images/pic01.jpg" alt="" />
-					</div>
-					<div class="flex-item right">
-						<div>
-							<h3>Tempus nullam</h3>
-							<p>Sed adipiscing ornare risus. Morbi estes<br /> blandit sit et amet, sagittis magna.</p>
-						</div>
-						<div>
-							<h3>Suscipit nibh dolore</h3>
-							<p>Pellentesque egestas sem. Suspendisse<br /> modo ullamcorper feugiat lorem.</p>
-						</div>
-					</div>
-				</div>
-			</section>
+    <!-- Bootstrap Core CSS -->
+    <link href="{{ asset('leohub/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
-		<!-- Two -->
-			<section id="two" class="wrapper style1 special">
-				<div class="inner">
-					<h2>Feugiat lorem</h2>
-					<figure>
-					    <blockquote>
-					        "Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra<br /> magna etiam lorem ultricies in diam. Sed arcu cras consequat."
-					    </blockquote>
-					    <footer>
-					        <cite class="author">Jane Anderson</cite>
-					        <cite class="company">CEO, Untitled</cite>
-					    </footer>
-					</figure>
-				</div>
-			</section>
+    <!-- Custom Fonts -->
+    <link href="{{ asset('leohub/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="{{ asset('leohub/vendor/simple-line-icons/css/simple-line-icons.css') }}" rel="stylesheet">
 
-		<!-- Three -->
-			<section id="three" class="wrapper">
-				<div class="inner flex flex-3">
-					<div class="flex-item box">
-						<div class="image fit">
-							<img src="images/pic02.jpg" alt="" />
-						</div>
-						<div class="content">
-							<h3>Consequat</h3>
-							<p>Placerat ornare. Pellentesque od sed euismod in, pharetra ltricies edarcu cas consequat.</p>
-						</div>
-					</div>
-					<div class="flex-item box">
-						<div class="image fit">
-							<img src="images/pic03.jpg" alt="" />
-						</div>
-						<div class="content">
-							<h3>Adipiscing</h3>
-							<p>Morbi in sem quis dui placerat Pellentesque odio nisi, euismod pharetra lorem ipsum.</p>
-						</div>
-					</div>
-					<div class="flex-item box">
-						<div class="image fit">
-							<img src="images/pic04.jpg" alt="" />
-						</div>
-						<div class="content">
-							<h3>Malesuada</h3>
-							<p>Nam dui mi, tincidunt quis, accu an porttitor, facilisis luctus que metus vulputate sem magna.</p>
-						</div>
-					</div>
-				</div>
-			</section>
+    <!-- Custom CSS -->
+    <link href="{{ asset('leohub/css/stylish-portfolio.min.css') }}" rel="stylesheet">
 
-		<!-- Footer -->
-			<footer id="footer">
-				<div class="inner">
-					<h2>Get In Touch</h2>
-					<ul class="actions">
-						<li><span class="icon fa-phone"></span> <a href="#">(000) 000-0000</a></li>
-						<li><span class="icon fa-envelope"></span> <a href="#">information@untitled.tld</a></li>
-						<li><span class="icon fa-map-marker"></span> 123 Somewhere Road, Nashville, TN 00000</li>
-					</ul>
-				</div>
-				<div class="copyright">
-					&copy; Untitled. Design <a href="https://templated.co">TEMPLATED</a>. Images <a href="https://unsplash.com">Unsplash</a>.
-				</div>
-			</footer>
+  </head>
 
-		<!-- Scripts -->
-            <script src="{{ asset('leohub/assets/js/jquery.min.js') }}"></script>
-            <script src="{{ asset('leohub/assets/js/jquery.scrolly.min.js') }}"></script>
-            <script src="{{ asset('leohub/assets/js/skel.min.js') }}"></script>
-            <script src="{{ asset('leohub/assets/js/util.js') }}"></script>
-            <script src="{{ asset('leohub/assets/js/main.js') }}"></script>
+  <body id="page-top">
 
-	</body>
+    <!-- Navigation -->
+    <a class="menu-toggle rounded" href="#">
+      <i class="fas fa-bars"></i>
+    </a>
+    <nav id="sidebar-wrapper">
+      <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+          <a class="js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#page-top">Home</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#about">About</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#services">Services</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#portfolio">Portfolio</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead d-flex">
+      <div class="container text-center my-auto">
+        <h1 class="mb-1">LeoHub</h1>
+        <h3 class="mb-5">
+          <em>Portfolio Online</em>
+        </h3>
+        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Descubrir mas</a>
+      </div>
+      <div class="overlay"></div>
+    </header>
+
+    <!-- About -->
+    <section class="content-section bg-light" id="about">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h2>LeoHub es </h2>
+            <p class="lead mb-5">un lugar donde puedes realizar tus grandes ideas, y lograr tus metas!</p>
+            <a class="btn btn-dark btn-xl js-scroll-trigger" href="#services">Que ofrecemos?</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Services -->
+    <section class="content-section bg-primary text-white text-center" id="services">
+      <div class="container">
+        <div class="content-section-heading">
+          <h3 class="text-secondary mb-0">Servicios</h3>
+          <h2 class="mb-5">Que ofrecemos?</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-screen-smartphone"></i>
+            </span>
+            <h4>
+              <strong>Responsive</strong>
+            </h4>
+            <p class="text-faded mb-0">Paginas Web visible desde cualquier dispositivo!</p>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-pencil"></i>
+            </span>
+            <h4>
+              <strong>Personalización</strong>
+            </h4>
+            <p class="text-faded mb-0">Proyectos a la medida.</p>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-like"></i>
+            </span>
+            <h4>
+              <strong>UX</strong>
+            </h4>
+            <p class="text-faded mb-0">La mejor experiencia para tus usuarios!</p>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <span class="service-icon rounded-circle mx-auto mb-3">
+              <i class="icon-mustache"></i>
+            </span>
+            <h4>
+              <strong>Moderno</strong>
+            </h4>
+            <p class="text-faded mb-0">Un diseño moderno a tu gusto!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Callout -->
+    <section class="callout">
+      <div class="container text-center">
+        <h2 class="mx-auto mb-5">Construimos
+          <em>tu</em>
+          proximo proyecto!</h2>
+        <a class="btn btn-primary btn-xl" href="#contact">Contactanos</a>
+      </div>
+    </section>
+
+    <!-- Conocimientos -->
+    <section class="content-section bg-primary text-white text-center" id="services">
+      <div class="container">
+        <div class="content-section-heading">
+          <h2 class="mb-5">Conocimientos</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-md-6 mb-5">
+            <span class="service-icon mx-auto mb-2">
+              <i class="fab fa-html5"></i>
+            </span>
+            <h4>
+              <strong>Html5</strong>
+            </h4>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <span class="service-icon mx-auto mb-2">
+              <i class="fab fa-php"></i>
+            </span>
+            <h4>
+              <strong>PHP(Senior)</strong>
+            </h4>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <span class="service-icon mx-auto mb-2">
+              <i class="fab fa-laravel"></i>
+            </span>
+            <h4>
+              <strong>Laravel & CodeIgniter</strong>
+            </h4>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <span class="service-icon mx-auto mb-2">
+              B
+            </span>
+            <h4>
+              <strong>Bootstrap</strong>
+            </h4>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <span class="service-icon mx-auto mb-2">
+              MySql
+            </span>
+            <h4>
+              <strong>PhpMyAdmin</strong>
+            </h4>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <span class="service-icon mx-auto mb-2">
+              NoSql
+            </span>
+            <h4>
+              <strong>MongoDb</strong>
+            </h4>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <span class="service-icon mx-auto mb-2">
+              <i class="fas fa-camera-retro"></i>
+            </span>
+            <h4>
+              <strong>Photoshop(Basico)</strong>
+            </h4>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+            <span class="service-icon mx-auto mb-2">
+              <i class="fab fa-wordpress"></i>
+            </span>
+            <h4>
+              <strong>Wordpress</strong>
+            </h4>
+          </div>          
+          <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+            <span class="service-icon mx-auto mb-2">
+              <i class="fab fa-bitcoin"></i>
+            </span>
+            <h4>
+              <strong>Altcoin Dev</strong>
+            </h4>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <span class="service-icon mx-auto mb-2">
+              <i class="fab fa-css3-alt"></i>
+            </span>
+            <h4>
+              <strong>CSS3</strong>
+            </h4>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <span class="service-icon mx-auto mb-2">
+              <i class="fab fa-git"></i>
+            </span>
+            <h4>
+              <strong>Git</strong>
+            </h4>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <span class="service-icon mx-auto mb-2">
+              <i class="fab fa-js"></i>
+            </span>
+            <h4>
+              <strong>JavaScript/Jquery</strong>
+            </h4>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <span class="service-icon mx-auto mb-2">
+            <i class="fab fa-angular"></i>
+            </span>
+            <h4>
+              <strong>Angular(Junior)</strong>
+            </h4>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <span class="service-icon mx-auto mb-2">
+              <i class="fab fa-node-js"></i>
+            </span>
+            <h4>
+              <strong>NodeJs(Junior)</strong>
+            </h4>
+          </div>
+          
+        </div>
+      </div>
+    </section>
+
+    <!-- Portfolio -->
+    <section class="content-section" id="portfolio">
+      <div class="container">
+        <div class="content-section-heading text-center">
+          <h3 class="text-secondary mb-0">Portfolio</h3>
+          <h2 class="mb-5">Proyectos Recientes</h2>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <span class="caption">
+                <span class="caption-content">
+                  <h2>SocialRobot</h2>
+                  <p class="mb-0">Robot de Trading de Criptomonedas Social - Trabaja con la API de Bittrex hecho en PHP-Laravel</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="{{ asset('leohub/img/portfolio-1.jpg') }}" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <span class="caption">
+                <span class="caption-content">
+                  <h2>Ice Cream</h2>
+                  <p class="mb-0">A dark blue background with a colored pencil, a clip, and a tiny ice cream cone!</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="{{ asset('leohub/img/portfolio-2.jpg') }}" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <span class="caption">
+                <span class="caption-content">
+                  <h2>Strawberries</h2>
+                  <p class="mb-0">Strawberries are such a tasty snack, especially with a little sugar on top!</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="{{ asset('leohub/img/portfolio-3.jpg') }}" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="#">
+              <span class="caption">
+                <span class="caption-content">
+                  <h2>Workspace</h2>
+                  <p class="mb-0">A yellow workspace with some scissors, pencils, and other objects.</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="{{ asset('leohub/img/portfolio-4.jpg') }}" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <!-- Call to Action -->
+    <section class="content-section bg-primary text-white">
+      <div class="container text-center">
+        <h2 class="mb-4">The buttons below are impossible to resist...</h2>
+        <a href="#" class="btn btn-xl btn-light mr-4">Click Me!</a>
+        <a href="#" class="btn btn-xl btn-dark">Look at Me!</a>
+      </div>
+    </section>
+
+    <!-- Map -->
+    <section id="contact" class="map">
+      <iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
+      <br/>
+      <small>
+        <a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;aq=0&amp;oq=twitter&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq=Twitter,+Inc.,+Market+Street,+San+Francisco,+CA&amp;t=m&amp;z=15&amp;iwloc=A"></a>
+      </small>
+    </section>
+
+    <!-- Footer -->
+    <footer class="footer text-center">
+      <div class="container">
+        <ul class="list-inline mb-5">
+          <li class="list-inline-item">
+            <a class="social-link rounded-circle text-white mr-3" href="#">
+              <i class="icon-social-facebook"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="social-link rounded-circle text-white mr-3" href="#">
+              <i class="icon-social-twitter"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="social-link rounded-circle text-white" href="#">
+              <i class="icon-social-github"></i>
+            </a>
+          </li>
+        </ul>
+        <p class="text-muted small mb-0">Copyright &copy; LeoHub 2018</p>
+      </div>
+    </footer>
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded js-scroll-trigger" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{ asset('leohub/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('leohub/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="{{ asset('leohub/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="{{ asset('leohub/js/stylish-portfolio.min.js') }}"></script>
+
+  </body>
+
 </html>
